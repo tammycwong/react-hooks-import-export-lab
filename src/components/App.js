7971307c3076ca0ea94cfdeedddcb133133a8bbe
Tammy <tammycwong@gmail.com>
2021-04-05 17:32:56 -0400

    finished with help

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Home from "../components/Home";
+import About from "../components/About";
+import NavBar from "../components/NavBar"
 
 function App() {
   return (
